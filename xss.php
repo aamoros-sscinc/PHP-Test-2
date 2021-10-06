f4770106ef946b97188895553ad91b57867a2c0f
aamoros-sscinc <90621947+aamoros-sscinc@users.noreply.github.com>
2021-10-06 10:58:55 -0400

    Update xss with code

--- xss.php
+++ xss.php
@@ -1,1 +1,12 @@
+<?php // --taint-analysis
 
+function getName() : string {
+    return $_GET['name'] ?? 'unknown';
+}
+
+function sayHello() : string {
+    return 'Hello ' . getName();
+}
+?>
+<!-- wrap call in htmlentities() to fix -->
+<h1><?= sayHello() ?></h1>
