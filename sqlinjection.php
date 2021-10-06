63f05cd60e0656fb509d63e813c71960be2ac5a6
aamoros-sscinc <90621947+aamoros-sscinc@users.noreply.github.com>
2021-10-06 10:59:58 -0400

    Added vulnerable code

--- sqlinjection.php
+++ sqlinjection.php
@@ -1,1 +1,20 @@
+<?php // --taint-analysis
 
+/** @psalm-immutable */
+class User {
+    public $id;
+    public function __construct($userId) {
+        $this->id = $userId;
+    }
+}
+
+class UserUpdater {
+    public static function deleteUser(PDO $pdo, User $user) : void {
+        $pdo->exec("delete from users where user_id = " . $user->id);
+    }
+}
+
+$userObj = new User($_GET["user_id"]);
+
+// remove the next line to fix issue
+UserUpdater::deleteUser(new PDO(), $userObj);
